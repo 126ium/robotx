20a8b49fc39934dc448aa288c8fcb151c65a25cf
Connor Yan <126ium@gmail.com>
2015-10-22 15:42:21 +0800

    revise syntex in tests/test_run.py
    Signed-off-by: Connor Yan <126ium@gmail.com>

--- tests/test_run.py
+++ tests/test_run.py
@@ -9,6 +9,7 @@ Usage: python runtests.py
 import os
 import tempfile
 import unittest
+import shutil
 import traceback
 
 from robot import run
@@ -18,13 +19,15 @@ class RunTestCase(unittest.TestCase):
         self.test_path = os.path.curdir
         tmp_path = tempfile.gettempdir()
         self.output_dir = os.path.join(tmp_path, "robotx_test_result")
+        if (os.path.exists(self.output_dir)):
+            shutil.rmtree(self.output_dir)
         os.mkdir(self.output_dir)
         self.output_file = os.path.join(self.output_dir, "output.xml")
         self.log_file = os.path.join(self.output_dir, "log.html")
         self.report_file = os.path.join(self.output_dir, "report.html")
         self.suites = ["test_cmd_*"]
 
-    def test_run():
+    def test_run(self):
         try:
             run(self.test_path,
                 suite=self.suites,
